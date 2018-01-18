412fc89dac7f449810de20d7aca6fc6291d1d641
Winton Welsh <mail@wintoni.us>
2018-01-18 11:24:50 -0500

    All functions implemented

--- test/index.test.js
+++ test/index.test.js
@@ -10,3 +10,35 @@ test("get", () => {
   expect(dot.get({fooBarBuzz: 'unicorn'}, 'foo.bar.buzz'))
     .toBe("unicorn")
 })
+
+test("set", () => {
+  expect(dot.set({foo: {bar: 'a'}}, 'foo.bar', 'b'))
+    .toEqual({foo: {bar: 'b'}})
+
+  expect(dot.set({fooBar: 'a'}, 'foo.bar', 'b'))
+    .toEqual({fooBar: 'b'})
+})
+
+test("delete", () => {
+  expect(dot.delete({foo: {bar: 'a'}}, 'foo.bar'))
+    .toEqual({foo: {}})
+
+  expect(dot.delete({fooBar: 'a'}, 'foo.bar'))
+    .toEqual({})
+})
+
+test("toggle", () => {
+  expect(dot.toggle({foo: {bar: true}}, 'foo.bar'))
+    .toEqual({foo: {bar: false}})
+
+  expect(dot.toggle({fooBar: true}, 'foo.bar'))
+    .toEqual({fooBar: false})
+})
+
+test("merge", () => {
+  expect(dot.merge({foo: {bar: {buzz: 'a'}}}, 'foo.bar', {bang: 'b'}))
+    .toEqual({foo: {bar: {buzz: 'a', bang: 'b'}}})
+
+  expect(dot.merge({fooBar: {buzz: 'a'}}, 'foo.bar', {bang: 'b'}))
+    .toEqual({fooBar: {buzz: 'a', bang: 'b'}})
+})
